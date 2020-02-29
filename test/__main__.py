d1ffc0f4f8ea0c8dae0ef9484004a641c01dc276
Serdar Yegulalp <serdar@genjipress.com>
2020-02-29 18:58:54 -0500

    add initial tests

---
+++ test/__main__.py
@@ -0,0 +1,149 @@
+import unittest
+
+# TODO: need image tests
+
+
+class TestRendering(unittest.TestCase):
+    def setUp(self):
+
+        self.models = models
+        self.author = self.models.Author.select().get()
+
+        self.wiki = self.models.Wiki(
+            title="New test wiki",
+            description="Wiki created for testing.",
+            author=self.author,
+        )
+        self.wiki.save()
+
+        self.article = self.models.Article(
+            wiki=self.wiki, title="Test data article", content="", author=self.author,
+        )
+        self.article.save()
+
+        with open(os.path.join("test", "testdata", "source.md")) as f:
+            self.article.content = f.read()
+        self.article.save()
+
+        self.include_article = self.models.Article(
+            wiki=self.wiki,
+            title="Include article",
+            content="**Formatted content**",
+            author=self.author
+        )
+        self.include_article.save()
+
+    def test_article_all_inclusive(self):
+        self.maxDiff = None
+        content = self.article.formatted
+        with open(os.path.join("test", "testdata", "output.html")) as f:
+            output = f.read()
+        self.assertEqual(
+            content, output,
+        )
+
+    def _make_article(self, article_title):
+        article = self.models.Article(
+            wiki=self.wiki, title=article_title, content="", author=self.author,
+        )
+        article.save()
+        return article
+
+    def test_article_delete(self):
+        article_title = "Test article for deletion"
+        test_article = self._make_article(article_title)
+        test_article.delete_instance(recursive=True)
+        self.assertEqual(
+            self.wiki.articles.where(
+                self.models.Article.title == article_title
+            ).count(),
+            0,
+        )
+
+    def test_url_conversions(self):
+        base = self.models.BaseModel
+        
+        self.assertEqual(base.title_to_url('"Title with quotes"'), "%22Title_with_quotes%22")
+        
+        self.assertEqual(base.url_to_title("%22Title_with_quotes%22"), '"Title with quotes"')
+
+        self.assertEqual(base.title_to_url('Title_with_underscores'), "Title_with_underscores")
+
+        self.assertEqual(base.url_to_title('Title_with_underscores'), "Title with underscores")        
+        
+        self.assertEqual(
+            base.title_to_url("Fall Of The Hammer / Original Version"),
+            "Fall_Of_The_Hammer_%2f_Original_Version",
+        )
+        self.assertEqual(
+            base.url_to_title("Fall_Of_The_Hammer_%2f_Original_Version"),
+            "Fall Of The Hammer / Original Version",
+        )
+        self.assertEqual(base.file_to_url("myfile_01 ext.jpg"), "myfile_01%20ext.jpg")
+        self.assertEqual(base.url_to_file("myfile_01%20ext.jpg"), "myfile_01 ext.jpg")
+
+
+    def test_article_exists(self):
+        self.assertTrue(
+            self.wiki.article_exists("Test data article"))
+        self.assertFalse(
+            self.wiki.article_exists("Nonexistent article"))
+
+    def test_tag_exists(self):
+        self.article.add_tag("Foobletzky")
+        self.assertTrue(self.wiki.tag_exists('Foobletzky'))
+        self.assertFalse(self.wiki.tag_exists('Veeblefetzer'))
+        self.article.remove_tag("Foobletzky")
+        self.assertFalse(self.wiki.tag_exists('Foobletzky'))
+
+    def test_wiki_has_name_collision(self):
+        new_wiki = self.models.Wiki(title="New test wiki", description="")
+        self.assertTrue(new_wiki.has_name_collision())
+        new_wiki.title="Non-colliding test wiki"
+        self.assertFalse(new_wiki.has_name_collision())
+
+    def test_wiki_link(self):
+        self.assertEqual(self.wiki.link,'/wiki/New_test_wiki')
+
+    def test_wiki_new_page_link(self):
+        self.assertEqual(self.wiki.new_page_link,'/wiki/New_test_wiki/new')
+
+    def test_wiki_edit_link(self):
+        self.assertEqual(self.wiki.edit_link,'/wiki/New_test_wiki/edit')
+
+    def test_wiki_search_link(self):
+        self.assertEqual(self.wiki.search_link,'/wiki/New_test_wiki/search')
+
+    def test_wiki_upload_link(self):
+        self.assertEqual(self.wiki.upload_link,'/wiki/New_test_wiki/upload')
+
+    def test_wiki_media_link(self):
+        self.assertEqual(self.wiki.media_link,'/wiki/New_test_wiki/media')
+
+    def test_default_wiki(self):
+        new_wiki = self.models.Wiki.default()
+        self.assertEqual(new_wiki.title, 'Untitled Wiki')
+        self.assertEqual(new_wiki.description, 'Replace this with your description.')
+
+
+if __name__ == "__main__":
+
+    import sys, os
+
+    sys.path.insert(0, os.path.join("test", "testdata"))
+    sys.path.insert(0, "folio")
+    import models as m
+
+    global models
+    models = m
+    models.create_db()
+
+    try:
+        unittest.main(failfast=True)
+    except:
+        pass
+
+    # teardown
+    models.db.close()
+    os.remove(os.path.join("test", "testdata", "wiki.db"))
+    os.rmdir(os.path.join("test", "testdata", "1"))
---
+++ test/testdata/source.md
@@ -0,0 +1,69 @@
+*Italic* text
+
+**Bold** text
+
+***Bold ital*** text
+
+~~Strikethrough~~ text
+
+```
+Preformatted block
+[[test]]
+{{test}}
+```
+
+\<\<Escaped brackets\>\>
+
+\<a\>Escaped HTML\</a\>
+
+\{\{Escaped braces\}\}
+
+\[\[Escaped link\]\]
+
+{{Test article}}
+
+<<<Test article>>>
+
+Inline `code [[test]] {{test}}` inline
+
+# Level 1
+
+## Level 2
+
+### Level 3
+
+#### Level 4
+
+##### Level 5
+
+[Named external link](https://google.com)
+
+Direct external link: [[https://google.com]]
+
+Article link: [[Test page]]
+
+[Named article link](Test page)
+
+[[Nonexistent link]]
+
+[Nonexistent named link](Nonexistent link)
+
+Link to tag: [meta](/tag/meta)
+
+Link to nonexistent tag: [nonmeta](/tag/nonmeta)
+
+[ ] Unselected checkbox  
+[x] Selected checkbox
+
+Single line ...  
+... followed by linebreak
+
+Table test:
+
+|Artist|Song|Scene|
+|Skinny Puppy|Death|Credits|
+|Skinny Puppy|Candle|Alice|
+
+{{Include article}}
+
+{{{Include article}}}
