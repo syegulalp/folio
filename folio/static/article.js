9dd99cea77f421b11ac4e7d64592cc2673e9bfc1
Serdar Yegulalp <serdar@genjipress.com>
2020-03-07 12:18:23 -0500

    remove spurious debug lines

--- folio/static/article.js
+++ folio/static/article.js
@@ -1,9 +1,6 @@
 $(window).on("keydown", function (e) {
-console.log("a")
 if (e.ctrlKey) {
-    console.log("b")
     if (e.code == 'KeyE') {
-    console.log("OK")
     e.preventDefault();
     $('#article-edit-link')[0].click();
     }
