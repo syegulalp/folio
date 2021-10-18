c2b260cc2bb403ea82d4a593e1e6368d337839b1
Serdar Yegulalp <serdar@genjipress.com>
2021-10-18 13:03:22 -0400

    bugfix: oops, needs preventDefault

--- folio/static/homesearch.js
+++ folio/static/homesearch.js
@@ -1,5 +1,6 @@
 $("#wiki-search").on("keyup", function(e) {
     if (e.code == "Enter" & $("#wiki-search").val().length > 0) {
+        e.preventDefault();
         window.location = $("#wiki-listing").children().find("a")[0].href;
     } else {
         $.get(
